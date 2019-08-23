f1226ff11641e1746c263b08927360e0f7884432
Xuzheng <linxz@garena.com>
2019-08-23 11:34:16 +0800

    adjust date position, upload 2nd json

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@ var btn = document.getElementById('btn');
 var animalContainer = document.getElementById("animal-info");
 var newsListContainer = document.getElementById("news-list-container");
 let jsonPage = 1;
-btn.addEventListener("click", function(){
-    // var ourRequest = new XMLHttpRequest();
-    // ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+jsonPage+'.json');
 
-    /*As chrome forbid load local file, json is GET from github*/
+btn.addEventListener("click", function(){
+   /*As chrome forbid load local file, json is GET from github*/
     let ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://raw.githubusercontent.com/robinlxz/news_page_learning/master/json/test.json')
+    ourRequest.open('GET', 'https://raw.githubusercontent.com/robinlxz/news_page_learning/master/json/web.json')
     
-    jsonPage<=3 ? jsonPage++ : console.log("It's done.");
-    if (jsonPage>3) {
-        btn.classList.add("hide-me");
-    }
+    // if (jsonPage>3) {
+    //     btn.classList.add("hide-me");
+    // }
     animalContainer.insertAdjacentHTML('beforeend', "<br><br>")  
 
     ourRequest.onload = function () {
@@ -23,12 +20,12 @@ btn.addEventListener("click", function(){
             renderHTML(ourData);
         }
         else {
-            animalContainer.insertAdjacentHTML('beforeend', "Ops, server returned error code: "+ourRequest.status)
+            newsListContainer.insertAdjacentHTML('beforeend', "Ops, server returned error code: "+ourRequest.status)
         }
     };
 
     ourRequest.onerror = function () {
-        animalContainer.insertAdjacentHTML('beforeend', "Ops, connection error")
+        newsListContainer.insertAdjacentHTML('beforeend', "Ops, connection error")
     }
 
     ourRequest.send();
@@ -40,19 +37,26 @@ var renderHTML = function(data){
     // let htmlString = "<strong>I'm a html string!</strong><br>";
     let htmlString = '';
     for (let i=0; i<data.length; i++) {
-        htmlString +="<div>"
-        // htmlString += "<li>" + data[i].name + " is a " + data[i].species + "</li>";
-        // htmlString += "<li>" + data[i].name + " likes " + data[i].foods.likes[0] + "</li>";
-        // htmlString += "<br>";
-        htmlString += "<p>" + data[i].name + " is a " + data[i].species + "that likes to eat ";
-        for (let p=0; p<data[i].foods.likes.length; p++) {
-            htmlString += "<li>" + data[i].foods.likes[p] + "</li>"
-        }
-        
-        htmlString += "</p>" + "</div>";
+        htmlString += '<div class="horizontal-news-box">';
+            htmlString += '<a href="#">';
+            htmlString += '<img class="image-link" src="' + data[i].imgUrl + '" alt="news image">';
+            htmlString += '</a>';
+            //Above is picture, below is text.
+            htmlString += '<div class="newsinfo">';
+            htmlString += '<p class="category">' + data[i].category + '</p>';
+            // htmlString += '<p class="category">{{data[i].category}}</p>';
+            htmlString += '<a href="#" class="news-title">' + data[i].title + '</a>';
+            htmlString += '<p class="date">' + data[i].date + '</p>';
+            htmlString += '</div>'
+        htmlString += '</div>'
     }
-    animalContainer.insertAdjacentHTML('beforeend', htmlString)    
+    newsListContainer.insertAdjacentHTML('beforeend', htmlString)    
 };
+
+
+/*
+https://raw.githubusercontent.com/robinlxz/news_page_learning/master/json/web.json
+*/
 
 let renderHTML2 = function(importedJSON) {
     let htmlString = '';
